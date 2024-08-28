adfcc683f59a71cf2a18f23c6125940ca1380c26
Dyaoss <eogus12003@naver.com>
2024-08-29 00:36:04 +0900

    로그인,회원가입페이지 구조 변경

--- app/src/main/java/com/android/petopia/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/android/petopia/presentation/register/RegisterActivity.kt
@@ -1,20 +1,18 @@
-package com.android.petopia.presentation.signin
+package com.android.petopia.presentation.register
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.android.petopia.R
 import com.android.petopia.databinding.ActivitySigninBinding
 
-class SigninActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivitySigninBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
 
     }
 }
