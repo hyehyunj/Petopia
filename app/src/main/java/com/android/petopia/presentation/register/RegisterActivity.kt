169ea55039c2bfc073a1864a54417a8617008b8e
Dyaoss <eogus12003@naver.com>
2024-08-29 15:27:14 +0900

    회원가입페이지, 로그인 동작 구현

--- app/src/main/java/com/android/petopia/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/android/petopia/presentation/register/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.android.petopia.presentation.register
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.android.petopia.R
 import com.android.petopia.databinding.ActivityRegisterBinding
@@ -9,6 +10,8 @@ import com.android.petopia.presentation.register.signin.SigninFragment
 class RegisterActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityRegisterBinding.inflate(layoutInflater) }
+
+    private val registerViewModel: RegisterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/android/petopia/presentation/register/signin/SigninFragment.kt
+++ app/src/main/java/com/android/petopia/presentation/register/signin/SigninFragment.kt
@@ -1,12 +1,17 @@
 package com.android.petopia.presentation.register.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import com.android.petopia.R
 import com.android.petopia.databinding.FragmentSigninBinding
+import com.android.petopia.presentation.MainActivity
+import com.android.petopia.presentation.register.RegisterViewModel
 import com.android.petopia.presentation.register.signup.SignupFragment
 
 
@@ -14,6 +19,8 @@ class SigninFragment : Fragment() {
 
     private var _binding: FragmentSigninBinding? = null
     private val binding get() = _binding!!
+
+    private val registerViewModel: RegisterViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -27,8 +34,25 @@ class SigninFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         binding.btnSignin.setOnClickListener {
             //로그인버튼 누를시 로그인
+            val userInputId = binding.etSigninId.text.toString()
+            val userInputPassword = binding.etSigninPassword.text.toString()
+
+
+            if (registerViewModel.validateSignin(userInputId, userInputPassword)) {
+                //로그인 성공
+                Toast.makeText(requireContext(), "로그인 성공", Toast.LENGTH_SHORT).show()
+                //메인화면으로 이동
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+
+            } else {
+                //로그인 실패
+                Toast.makeText(requireContext(), "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
         }
 
         //회원가입 누르면 회원가입 프래그먼트로 이동
