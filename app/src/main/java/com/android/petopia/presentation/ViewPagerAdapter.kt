c672968510e7c4415fe36f155b319255023e5c28
hyehyunj <developh620@gmail.com>
2024-08-30 14:45:22 +0900

    feat: 가이드 다이얼로그 생성, 가이드 로직 개선

--- app/src/main/java/com/android/petopia/presentation/ViewPagerAdapter.kt
+++ app/src/main/java/com/android/petopia/presentation/ViewPagerAdapter.kt
@@ -5,12 +5,13 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.android.petopia.presentation.community.CommunityFragment
 import com.android.petopia.presentation.gallery.GalleryFragment
+import com.android.petopia.presentation.guide.GuideFragment
 import com.android.petopia.presentation.home.HomeFragment
 import com.android.petopia.presentation.my.MyFragment
 
 
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
-    private val fragments = listOf(HomeFragment(), CommunityFragment(), GalleryFragment())
+    private val fragments = listOf(HomeFragment(), CommunityFragment(), GuideFragment())
 
     override fun getItemCount(): Int {
         return fragments.size
