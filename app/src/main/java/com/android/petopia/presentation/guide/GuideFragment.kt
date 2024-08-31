f3abc966fb8b8ca4e4b397642f5b4aa261579a9e
hyehyunj <developh620@gmail.com>
2024-08-31 23:39:25 +0900

    pull 전 commit

--- app/src/main/java/com/android/petopia/presentation/guide/GuideFragment.kt
+++ app/src/main/java/com/android/petopia/presentation/guide/GuideFragment.kt
@@ -2,12 +2,15 @@ package com.android.petopia.presentation.guide
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.android.petopia.databinding.FragmentGuideBinding
+import com.android.petopia.databinding.ToastBinding
 
 //가이드 프래그먼트 : 앱 사용방법과 특징을 안내하는 튜토리얼
 class GuideFragment : Fragment() {
@@ -31,7 +34,8 @@ class GuideFragment : Fragment() {
         //다음으로 또는 뒤로가기 버튼 클릭에 따라 페이지 번호 변경
         guideViewModel.guidePageNumberLiveData.observe(viewLifecycleOwner) {
             Log.d("가이드는", "${guideViewModel.guidePageNumberLiveData.value}")
-            if (it in 0..7) guideViewModel.makeGuideModel()
+            if (it in 0..7) guideViewModel.makeGuideModel() else showToastMessage()
+
         }
 
         //페이지 번호에 따라 화면 구성
@@ -43,9 +47,11 @@ class GuideFragment : Fragment() {
                 0 -> GuideNameDialogFragment().apply {
                     isCancelable = false
                 }.show(childFragmentManager, "DIALOG_FRAGMENT")
+
                 1 -> GuideAppearanceDialogFragment().apply {
                     isCancelable = false
                 }.show(childFragmentManager, "DIALOG_FRAGMENT")
+
                 2 -> GuideRelationDialogFragment().apply {
                     isCancelable = false
                 }.show(childFragmentManager, "DIALOG_FRAGMENT")
@@ -65,5 +71,18 @@ class GuideFragment : Fragment() {
         return binding.root
     }
 
+
+    fun showToastMessage() {
+        val toastBinding = ToastBinding.inflate(layoutInflater)
+        toastBinding.run {
+            val toastOnCreateView = Toast(requireActivity())
+            toastOnCreateView.apply {
+                view = toastBinding.root
+                setGravity(Gravity.CENTER, 0, 400)
+                duration = Toast.LENGTH_SHORT
+                show()
+            }
+        }
+    }
 
 }
