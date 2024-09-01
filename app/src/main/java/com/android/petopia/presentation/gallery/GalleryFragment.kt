7f05438c9251a445e7c708a7903b06e6da04865a
hyehyunj <developh620@gmail.com>
2024-09-02 03:06:07 +0900

    chore: pull 전 commit

--- app/src/main/java/com/android/petopia/presentation/gallery/GalleryFragment.kt
+++ app/src/main/java/com/android/petopia/presentation/gallery/GalleryFragment.kt
@@ -42,7 +42,7 @@ class GalleryFragment : DialogFragment() {
         galleryButtonClickListener()
         //데이터 변화감지
         galleryDataObserver()
-        gallerySharedViewModel.loadGalleryList()
+//        gallerySharedViewModel.loadGalleryList()
 
 
         initDialog()
